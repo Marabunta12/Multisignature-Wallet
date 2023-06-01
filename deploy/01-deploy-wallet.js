a0119d5eedb6fca813295df3ec260983a86b760b
Bartek <s210869@sggw.edu.pl>
2023-06-01 20:40:39 +0200

    add deploy script

---
+++ deploy/01-deploy-wallet.js
@@ -0,0 +1,19 @@
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy } = deployments;
+    const { deployer } = await getNamedAccounts();
+
+    let args = [
+        [
+            "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+            "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
+        ],
+    ];
+
+    await deploy("MultiSignatureWallet", {
+        from: deployer,
+        args: args,
+        log: true,
+    });
+};
+
+module.exports.tags = ["MultiSignatureWallet"];
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-deploy");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+    solidity: "0.8.18",
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
 };
